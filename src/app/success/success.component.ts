5280e9e172605af7f43b5afb75fe572c468d8b0d
pallavibhandary <pallavibhandary@gmail.com>
2024-02-26 12:19:07 +0530

    new components

---
+++ src/app/success/success.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  template: `<p>This was successfully implemented</p>`,
+  styles: `
+    p{
+      color:green;
+    }`
+})
+export class SuccessComponent {
+
+}
